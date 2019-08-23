d72a02ecbefb8ffa221307a585f5e622f1dc607c
Sebastian Higuita <sebastianhc1995@hotmail.com>
2019-08-23 05:41:27 -0500

    Base configuration

---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default () => (
+    <header className="bg-gray-300">
+        <div className="container mx-auto p-12 max-w-4xl flex justify-center items-center">
+            <div className="flex-1">
+                <h1>¡Hola! Soy Sebastian</h1>
+                <p>Creo aplicaciones web</p>
+            </div>
+            <img></img>
+        </div>
+    </header>
+)
